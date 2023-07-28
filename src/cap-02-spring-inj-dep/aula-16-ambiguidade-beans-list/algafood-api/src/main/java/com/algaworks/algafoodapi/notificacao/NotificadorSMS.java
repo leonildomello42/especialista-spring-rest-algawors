be7d49a032a530dedce115f4bc6e6b17a78a663d
leonildomello42 <leonildo.mello@gmail.com>
2023-07-27 23:26:14 -0300

    Aula-16: ambiguidade de beans List

---
+++ src/cap-02-spring-inj-dep/aula-16-ambiguidade-beans-list/algafood-api/src/main/java/com/algaworks/algafoodapi/notificacao/NotificadorSMS.java
@@ -0,0 +1,15 @@
+package com.algaworks.algafoodapi.notificacao;
+
+
+import com.algaworks.algafoodapi.modelo.Cliente;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificadorSMS implements Notificador {
+
+    @Override
+    public void notificar(Cliente cliente, String mensagem) {
+        System.out.printf("Notificando %s por SMS através do telefone %s: %s\n",
+                cliente.getNome(), cliente.getTelefone(), mensagem);
+    }
+}
