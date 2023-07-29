7f3cd8665629bf73288400b9bf34cac95a1e7685
leonildomello42 <leonildo.mello@gmail.com>
2023-07-28 21:04:43 -0300

    Aula-28: Spring profiles

---
+++ src/cap-02-spring-inj-dep/aula-28-springs-profile/algafood-api/src/main/java/com/algaworks/algafoodapi/service/ClienteAtivadoEvent.java
@@ -0,0 +1,16 @@
+package com.algaworks.algafoodapi.service;
+
+import com.algaworks.algafoodapi.modelo.Cliente;
+
+public class ClienteAtivadoEvent {
+
+    private Cliente cliente;
+
+    public ClienteAtivadoEvent(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+}
