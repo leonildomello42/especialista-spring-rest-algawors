61381ebfcda7ca715e81fa8695682188dbe7fb98
leonildomello42 <leonildo.mello@gmail.com>
2023-07-29 16:28:55 -0300

    Aula-03: Adicionando JPA

---
+++ src/cap-03-intro-jpa-hibernate/aula-03-adicionando-jpa/algafood-api/src/main/java/com/algaworks/algafoodapi/notificacao/NotificadorSMS.java
@@ -0,0 +1,19 @@
+package com.algaworks.algafoodapi.notificacao;
+
+
+import com.algaworks.algafoodapi.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@TipoDoNotificador(value = NivelUrgencia.NORMAL)
+@Component
+public class NotificadorSMS implements Notificador {
+
+    @Override
+    public void notificar(Cliente cliente, String mensagem) {
+        System.out.printf("Notificando %s por SMS através do telefone %s: %s\n",
+                cliente.getNome(), cliente.getTelefone(), mensagem);
+    }
+}
